0719009e3013d33464612f69c03fe37441c3aafe
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2022-07-06 21:18:41 +0200

    express server test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hey")
+import * as express from "express";
+import * as wss from "ws";
+
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
