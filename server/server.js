2480971914099ac7c55b9e80148567691e30efcc
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2022-07-08 16:58:14 +0200

    Server and Client class

--- server/server.js
+++ server/server.js
@@ -3,36 +3,63 @@ import http from "http";
 import express from "express";
 import path from "path";
 import { dirname } from "path";
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+// command: $NODE_DEBUG=server npm start
+// to see debug messages
+import util from "util";
+let debug = util.debuglog("server");
 
-const app = express();
-const server = http.createServer(app);
-const io = geckos();
-const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use('/', express.static(path.join(__dirname, '../client')))
+class Server {
+  constructor() {
+    debug("Initializing Server");
+    this.app = express();
+    this.server = http.createServer(this.app);
+    this.io = geckos();
+    this.port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'))
-})
+    this.app.use("/", express.static(path.join(__dirname, "../client")));
 
-io.addServer(server);
-io.onConnection((channel) => {
-  channel.onDisconnect(() => {
-    console.log(`${channel.id} got disconnected`);
-  });
+    this.app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "../index.html"));
+    });
 
-  channel.on("chat message", (data) => {
-    console.log(`got ${data} from "chat message"`);
-    // emit the "chat message" data to all channels in the same room
-    io.room(channel.roomId).emit("chat message", data);
-  });
-});
-// make sure the client uses the same port
-// @geckos.io/client uses the port 9208 by default
-server.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`)
-});
+    this.io.addServer(this.server);
+    this.io.onConnection((channel) => {
+      channel.onDisconnect(() => {
+        debug(`${channel.id} got disconnected`);
+      });
+
+      channel.on("chat message", (data) => {
+        debug(`got ${data} from "chat message"`);
+        // emit the "chat message" data to all channels in the same room
+        this.io.room(channel.roomId).emit("chat message", data);
+      });
+    });
+    // make sure the client uses the same port
+    // @geckos.io/client uses the port 9208 by default
+    this.server.listen(this.port, () => {
+      debug(`Listening on http://localhost:${this.port}`);
+    });
+
+    this.Test();
+  }
+
+  Ping() {
+    debug("Sending ping");
+    this.io.emit("ping", "ping");
+  }
+
+  Test() {
+    setInterval(() => {
+      let messageNumber = Math.floor(Math.random() * 10000);
+      debug(`Emitting message with number ${messageNumber}`);
+      this.io.emit("chat message", messageNumber);
+    }, 1000);
+  }
+}
+
+const server = new Server();
