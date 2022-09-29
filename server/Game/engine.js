70010274c96b5e02a9c44d1af5ef7d3b835c2f14
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2022-09-29 14:27:55 +0200

    Engine blueprint Comments

--- server/Game/engine.js
+++ server/Game/engine.js
@@ -1,3 +1,18 @@
 export default class Engine {
 
+    constructor(World) {
+        this.world = World;
+        // this.gameLoop();
+    }
+
+    gameLoop() {
+        setInterval(() => {
+           this.physicsUpdate(); 
+        }, 50);
+    }
+
+    physicsUpdate() {
+        // apply physics
+        let players = this.world.players;
+    }
 }
